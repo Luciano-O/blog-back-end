ec832f93d8142d3ce30f9814d42917823d7327ac
Luciano <oliveiraluck78@gmail.com>
2022-07-04 12:05:54 -0300

    PERSONAL CHANGES

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,25 +1,18 @@
 require('dotenv').config();
 
-const environment = process.env.NODE_ENV || 'test';
-
-const suffix = {
-  dev: '-dev',
-  development: '-dev',
-  test: '-test',
-};
-
 const options = {
-  host: process.env.MYSQL_HOST || process.env.HOSTNAME || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
-  database: 
-    `${process.env.MYSQL_DB_NAME || 'blogs-api'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || '1234',
-  dialect: 'mysql',
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  database: process.env.MYSQL_DB_NAME,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  dialect: process.env.DIALECT,
   dialectOptions: {
-    timezone: 'Z',
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
   },
-  logging: process.env.DEBUG !== 'false',
 };
 
 module.exports = {
