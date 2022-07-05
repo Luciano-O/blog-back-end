4257b09b647bccc2e50352af1d2b715423d62f80
Luciano <oliveiraluck78@gmail.com>
2022-07-05 13:04:53 -0300

    FIX: models

--- src/database/models/postCategory.js
+++ src/database/models/postCategory.js
@@ -20,14 +20,14 @@ module.exports = (sequelize, DataTypes) => {
     models.Category.belongsToMany(models.BlogPost,{
       as: 'blogPost',
       through: PostCategory,
-      foreignKey: 'postId',
-      otherKey: 'categoryId',
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
     });
     models.BlogPost.belongsToMany(models.Category, {
       as: 'categories',
       through: PostCategory,
-      foreignKey: 'categoryId',
-      otherKey: 'postId',
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
     });
   }
 
