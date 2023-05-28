afec48588a7943c057973ced56969d657e6c8f9d
Luciano Oliveira <oliveiraluck78@gmail.com>
2023-05-28 05:18:39 -0300

    Try fix 2

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -5,15 +5,10 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
-let sequelize = new Sequelize(process.env.URL, {
-  dialectOptions: {
-    ssl: {
-      require: false,
-    }
-  },
-});
+let sequelize = new Sequelize(config);
 
 fs
   .readdirSync(__dirname)
