8562787fb52171c3e46f6457e7db188f774df89b
Luciano Oliveira <oliveiraluck78@gmail.com>
2023-05-28 05:06:22 -0300

    UNDO UNDO

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -5,10 +5,17 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
-let sequelize = new Sequelize(config);
+let sequelize = new Sequelize(process.env.URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: false,
+    }
+  },
+});
 
 fs
   .readdirSync(__dirname)
